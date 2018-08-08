72119ea16f650a4ba75151107eaf7763cdb3d899
Nytheneals <nythes@gmail.com>
2018-08-08 01:14:17 -0400

    [Material Design] - Added material design layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Toggle from './Components/Toggle';
 import './App.css';
 
 class App extends Component {
@@ -10,9 +11,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Toggle>
+          <h1>ERROOOOR</h1>
+        </Toggle>
       </div>
     );
   }
