7d2bca325424b6fd13e8868eff3c1890fa866d14
Nytheneals <nythes@gmail.com>
2018-08-08 01:15:32 -0400

    [Render Props] Added a low level render prop

---
+++ src/Components/Toggle.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class Toggle extends Component {
+  state = {
+    toggle: false,
+  };
+
+  handleToggle = () => {
+    this.setState({ toggle: !this.state.toggle });
+  };
+  render() {
+    return (
+      <div>
+        <button className=" mdc-button mdc-button--raised" onClick={this.handleToggle}>
+          show/hide
+        </button>
+        {this.state.toggle && this.props.children}
+      </div>
+    );
+  }
+}
+
+export default Toggle;
