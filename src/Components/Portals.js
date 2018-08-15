1de40aa99951ecadaeffc804480b519e5ce3bbc1
Nytheneals <nythes@gmail.com>
2018-08-14 23:16:29 -0400

    [Portals] Added more portals

--- src/Components/Portals.js
+++ src/Components/Portals.js
@@ -7,8 +7,13 @@ const portalRoot = document.querySelector('#portal');
 export default class Portal extends Component {
   render() {
     return ReactDOM.createPortal(
-      <div className="modal-window">
+      <div className="mdc-card my-card mdc-card__actions">
+        <div className="mdc-card__media-content">Title</div>
         <Nathan />
+        <div className="mdc-card__action-buttons">
+          <button className="mdc-button mdc-card__action mdc-card__action--button">Read</button>
+          <button className="mdc-button mdc-card__action mdc-card__action--button">Bookmark</button>
+        </div>
       </div>,
       portalRoot
     );
