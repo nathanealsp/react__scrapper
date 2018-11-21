5702cb2b2a335c3ea99e1295b4e2680709233a11
Nytheneals <nythes@gmail.com>
2018-11-20 23:19:20 -0500

    Finished the Auto Complete

--- src/App.js
+++ src/App.js
@@ -2,8 +2,30 @@ import React, { Component } from 'react';
 import { Portal, Modal, ToggleRenderPropRevisted, Prod, OtherModal } from 'Components';
 import LoginPage from './Components/LoginPage';
 import AutoComplete from './Components/AutoComplete';
+import SelectBox from './Components/select-box/Selector';
+import Selector from './Components/select-box/Selector@';
+
 import './App.css';
 
+const data = [
+  'Alligator',
+  'Bask',
+  'Crocodilian',
+  'Death Roll',
+  'Eggs',
+  'Jaws',
+  'Reptile',
+  'Solitary',
+  'Tail',
+  'Wetlands',
+];
+
+const items = [
+  { value: 'United States', id: 1 },
+  { value: 'Great Britian', id: 21 },
+  { value: 'Mexico', id: 43 },
+  { value: 'Canada', id: 2 },
+];
 class App extends Component {
   render() {
     return (
@@ -39,20 +61,10 @@ class App extends Component {
         <LoginPage /> */}
         <h1>React Autocomplete Demo</h1>
         <h2>Start typing and experience the autocomplete wizardry!</h2>
-        <AutoComplete
-          suggestions={[
-            'Alligator',
-            'Bask',
-            'Crocodilian',
-            'Death Roll',
-            'Eggs',
-            'Jaws',
-            'Reptile',
-            'Solitary',
-            'Tail',
-            'Wetlands',
-          ]}
-        />
+        <AutoComplete suggestions={[...data]} />
+        {/* <SelectBox name="venue[country_id]" items={{ ...items }} /> */}
+
+        <Selector suggestions={[...data]} />
       </div>
     );
   }
